24ff066a69e30d2c078b85906a28fbf91d1f9114
Horlakz <horlakz@protonmail.com>
2022-02-12 17:47:36 +0100

    added functionality for the yes and no buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,42 @@
 const yesBtn = document.querySelector(".btn")
 const noBtn = document.querySelector(".btn1")
+const displayReply = document.querySelector(".text")
+const displayBtn = document.querySelector('.buttons')
 
 yesBtn.addEventListener("click", showOutput)
 noBtn.addEventListener("click", showNoOutput)
 
+
 function showOutput() {
-  const displayReply = document.querySelector(".main")
-  
   let p = document.createElement('p')
-  p.textContent = 'Thanks Boss'
-  displayReply.innerHTML =''
+  p.classList.add('h1')
+  p.textContent = 'Thanks Boss for giving me the job'
+  displayReply.innerHTML = ''
   displayReply.appendChild(p)
-  
-    return alert ("Thanks for giving me the Job, shey ki n deop akant details")
+
+  displayBtn.innerHTML = ''
+  let link = document.createElement('a')
+  link.classList.add('btn')
+  link.href = "https://www.google.com"
+  link.textContent = "Tell me"
+  displayBtn.appendChild(link)
 }
 
 function showNoOutput() {
-    return alert ("Please sir, why are you not giving me the Job, kindly reconsider")
+  let p = document.createElement('p')
+  p.classList.add('h1')
+  p.textContent = 'Boss, Please na'
+  displayReply.innerHTML = ''
+  displayReply.appendChild(p)
+
+  displayBtn.innerHTML = ''
+  let link = document.createElement('button')
+  link.classList.add('btn')
+  link.addEventListener('click', reloadPage)
+  link.textContent = "Go back and Click"
+  displayBtn.appendChild(link)
+}
+
+function reloadPage() {
+  window.location.reload()
 }
