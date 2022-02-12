82077b9e579923b704cb2be93e01789a07a46036
horlakz <horlakz@protonmail.com>
2022-02-12 14:46:09 +0100

    added js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,17 @@ const yesBtn = document.querySelector(".btn")
 const noBtn = document.querySelector(".btn1")
 
 yesBtn.addEventListener("click", showOutput)
-noBtn .addEventListener("click", showNoOutput)
+noBtn.addEventListener("click", showNoOutput)
 
 function showOutput() {
-    return alert ("Thanks for giving me the Job")
+  const displayReply = document.querySelector(".main")
+  
+  let p = document.createElement('p')
+  p.textContent = 'Thanks Boss'
+  displayReply.innerHTML =''
+  displayReply.appendChild(p)
+  
+    return alert ("Thanks for giving me the Job, shey ki n deop akant details")
 }
 
 function showNoOutput() {
